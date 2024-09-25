9de1f490eb6e463d7252596faa10621246d2c381
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-09-24 23:36:40 -0400

    trying to fix images for netlify

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <div id="footer">
-      <a href = "https://www.linkedin.com/in/matthew-williams-40117a19/" target = "_blank"><img src = "/src/assets/128px-LinkedIn_logo_initials.png" width = "25px" alt = "linked-in logo" /></a>
-      &emsp;<a href = "https://github.com/MatthewWilliamsCMH" target = "_blank"><img src = "/src/assets/github-mark.png" width = "25px" alt = "github logo" /></a>
-      &emsp;<a href = "https://stackoverflow.com" target = "_blank"><img src = "/src/assets/stack-overflow-icon.png" width = "25px" alt = "github logo" /></a>
+      <a href = "https://www.linkedin.com/in/matthew-williams-40117a19/" target = "_blank"><img src = "/128px-LinkedIn_logo_initials.png" width = "25px" alt = "linked-in logo" /></a>
+      &emsp;<a href = "https://github.com/MatthewWilliamsCMH" target = "_blank"><img src = "/github-mark.png" width = "25px" alt = "github logo" /></a>
+      &emsp;<a href = "https://stackoverflow.com" target = "_blank"><img src = "/stack-overflow-icon.png" width = "25px" alt = "github logo" /></a>
     </div>
   );
 }
